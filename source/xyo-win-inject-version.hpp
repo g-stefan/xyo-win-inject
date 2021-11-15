7b80899909750d2390be14827023be103b7c7ae5
Grigore Stefan <g_stefan@yahoo.com>
2021-11-15 23:10:21 +0200

    Update

--- source/xyo-win-inject-version.hpp
+++ source/xyo-win-inject-version.hpp
@@ -10,10 +10,10 @@
 #ifndef XYO_WIN_INJECT_VERSION_HPP
 #define XYO_WIN_INJECT_VERSION_HPP
 
-#define XYO_WIN_INJECT_VERSION_ABCD                 1,7,0,18
-#define XYO_WIN_INJECT_VERSION_STR                 "1.7.0"
-#define XYO_WIN_INJECT_VERSION_STR_BUILD           "18"
-#define XYO_WIN_INJECT_VERSION_STR_DATETIME        "2021-08-09 17:38:07"
+#define XYO_WIN_INJECT_VERSION_ABCD                 1,8,0,19
+#define XYO_WIN_INJECT_VERSION_STR                 "1.8.0"
+#define XYO_WIN_INJECT_VERSION_STR_BUILD           "19"
+#define XYO_WIN_INJECT_VERSION_STR_DATETIME        "2021-11-15 16:51:55"
 
 #ifndef XYO_RC
 
