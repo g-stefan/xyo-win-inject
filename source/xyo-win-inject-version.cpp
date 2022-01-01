66db4d3f2f374db49fd410d32759f0c94c077ac7
Grigore Stefan <g_stefan@yahoo.com>
2022-01-01 21:26:45 +0200

    Update

--- source/xyo-win-inject-version.cpp
+++ source/xyo-win-inject-version.cpp
@@ -14,10 +14,10 @@ namespace XYO {
 		namespace Inject {
 			namespace Version {
 
-				static const char *version_ = "1.9.0";
-				static const char *build_ = "19";
-				static const char *versionWithBuild_ = "1.9.0.19";
-				static const char *datetime_ = "2021-11-22 13:35:06";
+				static const char *version_ = "1.10.0";
+				static const char *build_ = "20";
+				static const char *versionWithBuild_ = "1.10.0.20";
+				static const char *datetime_ = "2022-01-01 21:19:47";
 
 				const char *version() {
 					return version_;
--- source/xyo-win-inject-version.hpp
+++ source/xyo-win-inject-version.hpp
@@ -10,10 +10,10 @@
 #ifndef XYO_WIN_INJECT_VERSION_HPP
 #define XYO_WIN_INJECT_VERSION_HPP
 
-#define XYO_WIN_INJECT_VERSION_ABCD                 1,9,0,19
-#define XYO_WIN_INJECT_VERSION_STR                 "1.9.0"
-#define XYO_WIN_INJECT_VERSION_STR_BUILD           "19"
-#define XYO_WIN_INJECT_VERSION_STR_DATETIME        "2021-11-22 13:35:06"
+#define XYO_WIN_INJECT_VERSION_ABCD                 1,10,0,20
+#define XYO_WIN_INJECT_VERSION_STR                 "1.10.0"
+#define XYO_WIN_INJECT_VERSION_STR_BUILD           "20"
+#define XYO_WIN_INJECT_VERSION_STR_DATETIME        "2022-01-01 21:19:47"
 
 #ifndef XYO_RC
 
